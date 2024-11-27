68ab0fc6c739d330d54e35f4b3eef85207c1746a
ArpitKrRespect <arpitkr2020@gmail.com>
2024-11-27 22:38:43 +0530

    Employee Data Issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
-import { getLocalStorage, setLocalStorage } from './utils/localStorage'
+import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
   const [user, setUser] = useState(null)
+  
   const handleLogin = (email,password) => {
-
     if(email=="admin@example.com" && password=="123456"){
       setUser('admin')
-
+      console.log('admin')
     }
     else if(email=="arpit@example.com" && password=="123456"){
       setUser('employee')
+      console.log('employee')
     }
     else
     alert("Invalid Credentials");
   }
-  console.log(user)
+
+  const data = useContext(AuthContext)
+  console.log(data)
+
   return (
     <>
     {!user ? <Login handleLogin={handleLogin}/> : ''}
